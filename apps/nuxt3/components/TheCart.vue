fdee53fa3296b40630fa68d3ce23941a681ac2e3
KreskoLab <redeezol@gmail.com>
2022-10-23 19:12:47 +0300

    refactor(Cart): move to separate folder

--- apps/nuxt3/components/TheCart.vue
+++ apps/nuxt3/components/TheCart.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AppSidebar from '@/components/App/AppSidebar.vue';
+import { AppSidebar } from '#components';
 
 const products = useCartProducts()
 const showCart = useCart()
