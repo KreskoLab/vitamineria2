fbd46d8e419fcb049a569c21de9f73e3d4e54365
KreskoLab <redeezol@gmail.com>
2022-11-11 12:20:17 +0200

    fix: footer

--- apps/nuxt3/components/TheFooter.vue
+++ apps/nuxt3/components/TheFooter.vue
@@ -1,10 +1,12 @@
 <template>
 	<footer class="fixed top-0 w-full bg-blue-gray-100 z-100">
 		<AppContainer class="flex items-center justify-between h-10 px-4 text-xs lg:(px-12 text-sm)">
-			<div class="flex space-x-6">
+			<div class="flex items-center space-x-6">
 				<div class="flex items-center space-x-1 lg:space-x-2">
 					<Icon name="tabler-phone" class="w-4 h-4" />
-					<span>+380 63 24 53 347</span>
+					<span>
+						+380 63 24 53 347
+					</span>
 				</div>
 	
 				<a
@@ -13,11 +15,13 @@
 					class="flex items-center space-x-1 lg:space-x-2"
 				>
 					<Icon name="tabler:brand-instagram" class="w-4 h-4" />
-					<span>vitamineria.ua</span>
+					<span class="hidden sm:inline-block">
+						vitamineria.ua
+					</span>
 				</a>
 			</div>
 
-			<NuxtLink class="font-semibold text-xl" to="/cooperation">
+			<NuxtLink class="font-semibold text-lg sm:text-xl" to="/cooperation">
 				🤝 Співпраця
 			</NuxtLink>
 		
