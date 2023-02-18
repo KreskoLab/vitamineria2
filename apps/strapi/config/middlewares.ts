38c1a47beb1fad77569ee8d9d74f2741545f1589
KreskoLab <redeezol@gmail.com>
2023-02-18 22:39:03 +0200

    bruh

--- apps/strapi/config/middlewares.ts
+++ apps/strapi/config/middlewares.ts
@@ -1,32 +1,32 @@
 export default [
-  'strapi::errors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      origin: [process.env.MERCHANT_RETURN_URL, process.env.ADMIN_URL, process.env.DEV_URL]
-    }
-  },
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+	'strapi::errors',
+	'strapi::poweredBy',
+	'strapi::logger',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
+	{
+		name: 'strapi::cors',
+		config: {
+			enabled: true,
+			headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+			origin: [process.env.MERCHANT_RETURN_URL, process.env.ADMIN_URL, process.env.DEV_URL]
+		}
+	},
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					'connect-src': ["'self'", 'https:'],
+					'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+					'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
 ];
