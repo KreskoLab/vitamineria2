692a60e58936cda554e6100312dd6372e1f53ed5
KreskoLab <redeezol@gmail.com>
2023-05-27 22:18:36 +0300

    fix

--- apps/nuxt3/queries/categories.ts
+++ apps/nuxt3/queries/categories.ts
@@ -2,7 +2,7 @@ import qs from 'qs'
 
 export const categoriesQuery = qs.stringify({
 	fields: ['name', 'slug'],
-	sort: ['priority:desc'],
+	sort: ['priority:asc'],
 	populate: {
 		subcategories: {
 			fields: ['name', 'slug', 'priority'],
