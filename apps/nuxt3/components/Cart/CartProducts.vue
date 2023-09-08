ecddb8b563300c3cbc26c4c5ed0c100e0275739e
KreskoLab <redeezol@gmail.com>
2023-09-08 16:13:10 +0300

    fix

--- apps/nuxt3/components/Cart/CartProducts.vue
+++ apps/nuxt3/components/Cart/CartProducts.vue
@@ -87,9 +87,10 @@ function toOrder() {
 
 				<button
 					class="w-full appearance-none border-2 border-gray-600 text-xl font-medium h-14 bg-green-200"
+					@click="toOrder"
 					
 				>
-					Замовити
+					Оформити
 				</button>
 			</div>
 		</section>
