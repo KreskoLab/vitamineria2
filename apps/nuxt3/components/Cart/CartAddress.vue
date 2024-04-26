9a4fbd3829575efcc05051aab07871939d732791
KreskoLab <redeezol@gmail.com>
2024-04-26 16:17:02 +0300

    fix

--- apps/nuxt3/components/Cart/CartAddress.vue
+++ apps/nuxt3/components/Cart/CartAddress.vue
@@ -285,6 +285,7 @@ watch(() => order.rozetka.city, async (val) => {
 				<button
 					:key="buttonKey" 
 					class="w-full appearance-none border-2 border-gray-600 text-xl font-medium h-14 bg-green-200"
+					@click="validate"
 				>					
 					<template v-if="isOnlinePayment">
 						Оплатити
